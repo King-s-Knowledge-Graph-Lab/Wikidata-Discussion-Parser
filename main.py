bdd99b6aaf88dcc8f0d5265a6f3f785e492e6d7e
IReklos <jreklos@gmail.com>
2022-11-08 18:58:31 +0000

    Added downloader functionality to find desired links in dump and a sample usage in main

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+from wikidataDownloader import Downloader
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main():
+    url = "https://dumps.wikimedia.org/wikidatawiki/20220801/"
+    wiki_downloader = Downloader(url=url)
+    wiki_downloader.get_links("meta")
+    print(wiki_downloader.links)
+    wiki_downloader.reset_downloader()
+    print(wiki_downloader.links)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- wikidataDownloader.py
+++ wikidataDownloader.py
@@ -1,26 +1,39 @@
 import os
-
 import statics
-import urllib
+import requests
+from bs4 import BeautifulSoup
+from typing import List
 
 
 class Downloader:
     save_dir: str
+    dump_url: str
+    urls_to_download: List[str]
 
-    def __init__(self, save_dir: str = None, ):
+    def __init__(self, save_dir: str = None, url: str = None):
         """
         The downloader class constructor
         :param save_dir: The path to the directory where the data will be saved
         """
         self.save_dir = save_dir if save_dir is not None else os.path.join(os.getcwd(), "Dump")
+        self.urls_to_download = []
         statics.check_and_make_dir(self.save_dir)
+        if url is not None:
+            self.dump_url = url
+        else:
+            raise ValueError('Invalid url. Please provide a valid wikidata dump url')
 
-    def get_links(self):
+    def get_links(self, data_type: str):
         """
-        A function that gets the links for the files of the desired wikidata dump
-        :return:
+        A function than finds the required links to the wikidata dump files
+        :param data_type: A string denoting which files are of interest (e.g. "meta")
+        :return: None
         """
-        pass
+        reqs = requests.get(self.dump_url)
+        soup = BeautifulSoup(reqs.text, 'html.parser')
+        for link in soup.find_all('a'):
+            if link.has_attr('href') and data_type in link['href'] and link['href'].split(".")[-1] == "bz2":
+                self.urls_to_download.append(link['href'])
 
     def download_file(self, link: str):
         """
@@ -29,3 +42,16 @@ class Downloader:
         """
         # urllib.request.urlretrieve(link, r'dumps_xml_Nov2021/num_' + str(n) + '.bz2')
         pass
+
+    def reset_downloader(self):
+        """
+        Resets the downloader object
+        :return: None
+        """
+        self.save_dir = ""
+        self.urls_to_download.clear()
+        self.dump_url = ""
+
+    @property
+    def links(self):
+        return self.urls_to_download
